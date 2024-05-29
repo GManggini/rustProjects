78c067e0594fee2bb1b40cbaa1a220c53d66cb98
Manggini <manggini.code@gmail.com>
2024-05-29 00:49:32 -0300

    adding my rust code

---
+++ guessing_game/target/debug/deps/cfg_if-cc9cb5b64779c561.d
@@ -0,0 +1,7 @@
+/home/manggini/rustprojects/guessing_game/target/debug/deps/libcfg_if-cc9cb5b64779c561.rmeta: /home/manggini/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/manggini/rustprojects/guessing_game/target/debug/deps/libcfg_if-cc9cb5b64779c561.rlib: /home/manggini/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/manggini/rustprojects/guessing_game/target/debug/deps/cfg_if-cc9cb5b64779c561.d: /home/manggini/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/manggini/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/manggini/rustprojects/guessing_game/target/debug/guessing_game: /home/manggini/rustprojects/guessing_game/src/main.rs
